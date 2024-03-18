41e2535a0429e15924516284a6389035183ec46c
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-03-18 14:18:35 -0700

    Add playRound function to javascript.js
    Created the playRound function to play a round of
    rock, paper, scissors with the user. Also edited
    some syntax in the getComputerChoice function.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,69 @@
 function getComputerChoice() {
+    // Create computerChoice variable
     let computerChoice;
+
+    // Generate random number from 0 - 2
     let randNum = Math.floor((Math.random() * 3));
+    
+    // Return a choice based on random number
     switch(randNum) {
         case 0:
             computerChoice = "Rock";
-            break
+            break;
         case 1:
             computerChoice = "Paper";
-            break
+            break;
         case 2:
             computerChoice = "Scissors"
-            break
+            break;
     }
     return computerChoice;
 }
+
+function playRound(playerSelection) {
+    // Get computer selection
+    let computerSelection = getComputerChoice();
+    
+    // Format playerSelection string
+    playerSelection = playerSelection.toLowerCase();
+    let capitalize = playerSelection.charAt(0);
+    capitalize = capitalize.toUpperCase();
+    playerSelection = playerSelection.slice(1);
+    playerSelection = capitalize + playerSelection;
+
+    // Determine game outcome and return verdict
+    switch(playerSelection) {
+        case "Rock":
+            if (computerSelection === "Paper") {
+                return `You lose! ${computerSelection} beats ${playerSelection}`;
+            }
+            else if (computerSelection === "Scissors") {        
+                return `You win! ${playerSelection} beats ${computerSelection}`;
+            }
+            else if (computerSelection === "Rock") {
+                return "It's a tie";
+            }
+        case "Paper":
+            if (computerSelection === "Scissors") {
+                return `You lose! ${computerSelection} beats ${playerSelection}`;
+            }
+            else if (computerSelection === "Rock") {        
+                return `You win! ${playerSelection} beats ${computerSelection}`;
+            }
+            else if (computerSelection === "Paper") {
+                return "It's a tie!";
+            }
+        case "Scissors":
+            if (computerSelection === "Rock") {
+                return `You lose! ${computerSelection} beats ${playerSelection}`;
+            }
+            else if (computerSelection === "Paper") {        
+                return `You win! ${playerSelection} beats ${computerSelection}`;
+            }
+            else if (computerSelection === "Scissors") {
+                return "It's a tie!"
+            }
+    }
+  }
+  
+  
